fcc8e14c36a915f6dc0c6e655e27b54f673143e9
Farhana Yasmeen <farhanayasmeen07@gmail.com>
2023-10-09 12:26:24 +0530

    Update in app.js 'changed the icons display if isArchived'

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 export function renderNotes(notes) {
-    let newNote = notes.map(({ id, note, title, isPinned, isArchived }) => {
-      return `<div class="single-note relative shadow">
+  let newNote = notes.map(({ id, note, title, isPinned, isArchived }) => {
+    return `<div class="single-note relative shadow">
                   <div class="d-flex align-center title-container">
                       <span class="single-note-title">${title}</span>
                       <button class="button del-btn v-hidden" data-type="del" data-id=${id}>
@@ -11,17 +11,18 @@ export function renderNotes(notes) {
                   <div class="options d-flex gap-md">
                       <button class="button btn pinned-btn v-hidden" data-type="pinned" data-id=${id}>
                           <span data-type="pinned" data-id=${id} class=${
-        isPinned ? "material-icons" : "material-icons-outlined"
-      }>push_pin</span>
+      isPinned ? "material-icons" : "material-icons-outlined"
+    }>push_pin</span>
                       </button>
                       <button class="button btn pinned-btn v-hidden" data-type="archive" data-id=${id}>
-                          <span data-type="archive" data-id=${id} class="material-icons-outlined">archive</span>
+                          <span data-type="archive" data-id=${id} class=${
+      isArchived ? "material-icons" : "material-icons-outlined"
+    }>archive</span>
                       </button>
                   </div>
               </div>`;
-    });
-  
-    newNote = newNote.join("");
-    return newNote;
-  }
-  
+  });
+
+  newNote = newNote.join("");
+  return newNote;
+}
